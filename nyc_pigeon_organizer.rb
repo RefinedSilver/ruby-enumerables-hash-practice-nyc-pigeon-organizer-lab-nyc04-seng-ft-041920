5e90bd5f7e9423bc11267f40443cf375f08acced
Ian McKenzie <imckenzie777@gmail.com>
2020-05-07 02:22:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -31,9 +31,9 @@ def nyc_pigeon_organizer(data)
           pigeons[name] = {}
         end
         if !new_hash[name][key]
-          new_hash[name][key] = []
+          pigeons[name][key] = []
         end
-        new_hash[name][key] << attrib.to_s
+        pigeons[name][key] << attrib.to_s
       end
     end
    end
