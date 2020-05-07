8400591f1ae1dea694d4292d763a6c5b909c751c
Ian McKenzie <imckenzie777@gmail.com>
2020-05-07 01:59:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -23,7 +23,7 @@ def nyc_pigeon_organizer(data)
     p "The c_g_l variable is equal to: #{color_gender_lives}"
     v_hash.each do |attrib, name_array|
       p "The attrib variable is equal to: #{attrib} and #{name_array}"
-      name_array.each do |k,v| 
+      name_array.each do |k,v|
         binding.pry
         p "The name_array variable is equal to: #{standard}"
       end
