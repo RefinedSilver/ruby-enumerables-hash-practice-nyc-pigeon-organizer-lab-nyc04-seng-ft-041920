67160ee8f7c5a1adf427972de60b4611afa2f766
Ian McKenzie <imckenzie777@gmail.com>
2020-05-07 02:26:27 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,45 @@
+require 'pry'
+pigeon_data = {
+  :color => { # attribute
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => { # attribute
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => { # attribute
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons = {}
+  data.each do |key, v_hash|
+    #p "The c_g_l variable is equal to: #{color_gender_lives}"
+    v_hash.each do |attrib, name_array|
+      #p "The attrib variable is equal to: #{attrib} and #{name_array}"
+      name_array.each do |name|
+      #  binding.pry
+        #p "The name_array variable is equal to: #{standard}"
+        if !pigeons[name]
+          pigeons[name] = {}
+        end
+        if !pigeons[name][key]
+          pigeons[name][key] = []
+        end
+        pigeons[name][key] << attrib.to_s
+      end
+    end
+   end
+   pigeons
 end
+# nyc_pigeon_organizer(pigeon_data)
+#Questions
+
+# On line 6. why do we need to reach into the nest
